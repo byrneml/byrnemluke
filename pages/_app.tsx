70d930c905b27caa69d02e2100b43a256eceb8f2
byrneml <byrne.m.luke@gmail.com>
2022-06-15 22:47:26 +0100

    theme update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <ThemeProvider
       storageKey="theme"
       enableSystem={true}
-      defaultTheme="dark"
+      defaultTheme="system"
       attribute="class"
     >
       <Component {...pageProps} />
