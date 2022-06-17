f8124f921806f598e445b53cf5e8c883bb6d8141
byrneml <byrne.m.luke@gmail.com>
2022-06-17 12:23:34 +0100

    updated theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,7 @@ import "../styles/global.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider
-      storageKey="theme"
-      enableSystem={true}
-      defaultTheme="system"
-      attribute="class"
-    >
+    <ThemeProvider storageKey="theme" defaultTheme="system" attribute="class">
       <Component {...pageProps} />
     </ThemeProvider>
   );
